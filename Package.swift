fe12bf6f3cd8ddd3248530ccaaf0b74c8f807cdb
Ali Ersoz <ali@mirror.co>
2020-09-18 13:52:59 -0400

    update for spm

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.0
+
+import PackageDescription
+
+let package = Package(
+        name: "YPImagePicker",
+        products: [
+            .library(name: "YPImagePicker", targets: ["YPImagePicker"])
+        ],
+        dependencies: [
+            .package(url: "https://github.com/freshOS/Stevia.git", from: "4.7.3"),
+            .package(url: "https://github.com/HHK1/PryntTrimmerView.git", from: "4.0.0")
+        ],
+        targets: [
+            .target(name: "YPImagePicker",
+                    path: "YPImagePicker")
+        ]
+)
