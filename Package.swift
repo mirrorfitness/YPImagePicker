f5723327a445076166e0b045ec26b9831aacf5f3
Ali Ersoz <ali@mirror.co>
2020-09-18 18:25:19 -0400

    remove stevia from package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,6 @@ let package = Package(
             .library(name: "YPImagePicker", targets: ["YPImagePicker"])
         ],
         dependencies: [
-            .package(url: "https://github.com/freshOS/Stevia.git", from: "4.7.3"),
             .package(url: "https://github.com/HHK1/PryntTrimmerView.git", from: "4.0.0")
         ],
         targets: [
